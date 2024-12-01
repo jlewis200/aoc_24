2c5021a919fec232f0d0caef63c0763c5c678997
Justin <18606523+jlewis200@users.noreply.github.com>
2024-12-01 09:30:24 -0500

    Day 1 part 2.

---
+++ 1/solve2.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+from re import fullmatch
+import numpy as np
+from collections import defaultdict
+
+
+def solve(list_0, list_1):
+    frequency = defaultdict(
+        lambda: 0,
+        zip(*np.unique(list_1, return_counts=True)),
+    )
+    similarity = 0
+
+    for element in list_0:
+        similarity += element * frequency[element]
+
+    return similarity
+
+
+def parse(lines):
+    list_0 = []
+    list_1 = []
+
+    for line in lines:
+        match = fullmatch("(?P<number_0>\d+)\s+(?P<number_1>\d+)", line.strip())
+        list_0.append(int(match.group("number_0")))
+        list_1.append(int(match.group("number_1")))
+
+    return list_0, list_1
+
+
+def read_file(filename):
+    with open(filename, encoding="utf-8") as f_in:
+        return f_in.readlines()
+
+
+def main(filename, expected=None):
+    result = solve(*parse(read_file(filename)))
+    print(result)
+    if expected is not None:
+        assert result == expected
+
+
+if __name__ == "__main__":
+    main("test.txt", 31)
+    main("input.txt")
