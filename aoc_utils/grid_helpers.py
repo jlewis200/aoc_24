27b377612365136656ea8806727bcc45a07db6ab
Justin <18606523+jlewis200@users.noreply.github.com>
2024-12-17 18:18:33 -0500

    Update grid string function.

--- aoc_utils/grid_helpers.py
+++ aoc_utils/grid_helpers.py
@@ -21,17 +21,21 @@ def expand_board(board, expansion_size=2):
     return np.repeat(board, expansion_size, axis=1)
 
 
+def parse(lines):
+    board = []
+
+    for line in lines:
+        board.append(list(line.strip()))
+
+    return np.array(board)
+
+
 def board_str(board):
     """
     Return the string representation of a numpy array where each element can be
     represented as a single character.
     """
-    string = ""
-    for row in board:
-        for col in row:
-            string += f"{col}"
-        string += "\n"
-    return string
+    return "\n".join("".join(row) for row in board)
 
 
 def hash_array(array):
