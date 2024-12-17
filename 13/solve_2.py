a0cd016d346bcf14d7a8f6a1f8d6bb8bacff5f48
Justin <18606523+jlewis200@users.noreply.github.com>
2024-12-17 10:41:06 -0500

    Day 13 part 2.

---
+++ 13/solve_2.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+from re import search
+from z3 import *
+
+
+def solve(games):
+    return sum(solve_game(game) for game in games)
+
+
+def solve_game(game):
+    (dy_a, dx_a), (dy_b, dx_b), (target_y, target_x) = game
+    target_y += 10000000000000
+    target_x += 10000000000000
+
+    presses_a, presses_b = Ints("presses_a presses_b")
+
+    s = Solver()
+    s.add(dy_a * presses_a + dy_b * presses_b == target_y)
+    s.add(dx_a * presses_a + dx_b * presses_b == target_x)
+
+    if s.check() == sat:
+        model = s.model()
+        cost = model[presses_a].as_long() * 3 + model[presses_b].as_long()
+        return cost
+
+    return 0
+
+
+def parse(data):
+    parsed = []
+
+    for game in data.split("\n\n"):
+        parsed.append(parse_game(game))
+
+    return parsed
+
+
+def parse_game(game):
+    match_a = search("Button A: X(?P<dx>.+), Y(?P<dy>.+)", game)
+    match_b = search("Button B: X(?P<dx>.+), Y(?P<dy>.+)", game)
+    match_target = search("Prize: X=(?P<x>.+), Y=(?P<y>.+)", game)
+
+    button_a = int(match_a.group("dy")), int(match_a.group("dx"))
+    button_b = int(match_b.group("dy")), int(match_b.group("dx"))
+    target = int(match_target.group("y")), int(match_target.group("x"))
+
+    return button_a, button_b, target
+
+
+def read_file(filename):
+    with open(filename, encoding="utf-8") as f_in:
+        return f_in.read()
+
+
+def main(filename, expected=None):
+    result = solve(parse(read_file(filename)))
+    print(result)
+    if expected is not None:
+        assert result == expected
+
+
+if __name__ == "__main__":
+    main("test_0.txt")
+    main("input.txt")
