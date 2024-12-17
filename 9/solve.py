30a73e15baa2eb9e54375897c3e5b0b8e914f0af
Justin <18606523+jlewis200@users.noreply.github.com>
2024-12-17 10:41:06 -0500

    Add day 9 part 1 solve.

--- 9/solve.py
+++ 9/solve.py
@@ -1,27 +1,53 @@
 #!/usr/bin/env python3
 
-import re
-import math
-import numpy as np
-import networkx as nx
+
+def solve(data):
+    idx = next_freespace(data, 0)
+
+    while idx < len(data):
+        item = data.pop(-1)
+
+        if item != -1:
+            data[idx] = item
+
+        idx = next_freespace(data, idx)
+
+    return get_checksum(data)
 
 
-def solve(parsed):
-    breakpoint()
+def next_freespace(data, idx):
+    while idx < len(data) and data[idx] != -1:
+        idx += 1
+
+    return idx
 
 
-def parse(lines):
+def get_checksum(data):
+    checksum = 0
+
+    for idx, value in enumerate(data):
+        checksum += idx * value
+
+    return checksum
+
+
+def parse(line):
+    line = list(line.strip())
     parsed = []
 
-    for line in lines:
-        parsed.append(line) # do something more useful here
-    
+    for data, idx in enumerate(range(0, len(line), 2)):
+        parsed += [data] * int(line[idx])
+        try:
+            parsed += [-1] * int(line[idx + 1])
+        except:
+            pass
+
     return parsed
 
 
 def read_file(filename):
     with open(filename, encoding="utf-8") as f_in:
-        return f_in.readlines()
+        return f_in.read()
 
 
 def main(filename, expected=None):
@@ -32,5 +58,5 @@ def main(filename, expected=None):
 
 
 if __name__ == "__main__":
-    main("test_0.txt", None)
+    main("test_0.txt", 1928)
     main("input.txt")
