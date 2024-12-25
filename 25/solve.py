dfe1eab6f3ea9f2ffe281588f0e6c437df9cec26
Justin <18606523+jlewis200@users.noreply.github.com>
2024-12-25 11:12:12 -0500

    Clean up implementation.

--- 25/solve.py
+++ 25/solve.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python3
 
 import numpy as np
+from itertools import product
 
 
 def solve(locks, keys):
     """
-    Add the lock and key together.  If no 2d arrray element is greater than 1,
-    then no overlap exists.
+    Add the 2d arrays of lock and key together.  If no element is greater than
+    1, then no overlap exists.
     """
     total = 0
 
-    for lock in locks:
-        for key in keys:
-            result = key + lock
-
-            if result.max() <= 1:
-                total += 1
+    for lock, key in product(locks, keys):
+        if (key + lock).max() <= 1:
+            total += 1
 
     return total
 
@@ -23,26 +21,36 @@ def solve(locks, keys):
 def parse(data):
     """
     Keys/locks are treated as integer arrays to facillitate overlap comparisons.
+
+    An array is a lock if every top element is "#" (mapped to 1), and a key if
+    every bottom element is "#" (mapped to 1).
     """
     locks = []
     keys = []
 
+    for array in parse_arrays(data):
+        if (array[0] == 1).all():
+            locks.append(array)
+
+        elif (array[-1] == 1).all():
+            keys.append(array)
+
+    return locks, keys
+
+
+def parse_arrays(data):
+    """
+    Parse input data into numpy arrays mapping "#" to 1 and "." to 0.
+    """
+    arrays = []
+
     for block in data.split("\n\n"):
         block = block.strip()
         lines = block.split("\n")
-        array = np.array(list(list(line) for line in lines))
+        array = np.array(list(map(list, lines)))
+        arrays.append((array == "#").astype(int))
 
-        # an array is a lock if every top element is "#"
-        if (array[0] == "#").all():
-            array = array == "#"
-            locks.append(array.astype(int))
-
-        # an array is a key if every bottom element is "#"
-        elif (array[-1] == "#").all():
-            array = array == "#"
-            keys.append(array.astype(int))
-
-    return locks, keys
+    return arrays
 
 
 def read_file(filename):
