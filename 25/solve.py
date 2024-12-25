f58cdbd9bb6b87785cb5bc7411a878402763f8b1
Justin <18606523+jlewis200@users.noreply.github.com>
2024-12-25 00:47:31 -0500

    Day 25.

--- 25/solve.py
+++ 25/solve.py
@@ -1,37 +1,62 @@
 #!/usr/bin/env python3
 
-import re
-import math
-import itertools
 import numpy as np
-import networkx as nx
 
 
-def solve(parsed):
-    breakpoint()
+def solve(locks, keys):
+    """
+    Add the lock and key together.  If no 2d arrray element is greater than 1,
+    then no overlap exists.
+    """
+    total = 0
+
+    for lock in locks:
+        for key in keys:
+            result = key + lock
+
+            if result.max() <= 1:
+                total += 1
+
+    return total
 
 
-def parse(lines):
-    parsed = []
+def parse(data):
+    """
+    Keys/locks are treated as integer arrays to facillitate overlap comparisons.
+    """
+    locks = []
+    keys = []
 
-    for line in lines:
-        parsed.append(line)  # do something more useful here
+    for block in data.split("\n\n"):
+        block = block.strip()
+        lines = block.split("\n")
+        array = np.array(list(list(line) for line in lines))
 
-    return parsed
+        # an array is a lock if every top element is "#"
+        if (array[0] == "#").all():
+            array = array == "#"
+            locks.append(array.astype(int))
+
+        # an array is a key if every bottom element is "#"
+        elif (array[-1] == "#").all():
+            array = array == "#"
+            keys.append(array.astype(int))
+
+    return locks, keys
 
 
 def read_file(filename):
     with open(filename, encoding="utf-8") as f_in:
-        return f_in.readlines()
+        return f_in.read()
 
 
 def main(filename, expected=None):
-    result = solve(parse(read_file(filename)))
+    result = solve(*parse(read_file(filename)))
     print(result)
     if expected is not None:
         assert result == expected
 
 
 if __name__ == "__main__":
-    main("test_0.txt", None)
+    main("test_0.txt", 3)
     main("input.txt")
