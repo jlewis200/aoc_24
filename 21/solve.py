81728cab7665dfda2b95ecac0a2ad33004f3ee3c
Justin <18606523+jlewis200@users.noreply.github.com>
2025-11-28 13:38:50 -0500

    Switch to AocDS helpers.

--- 21/solve.py
+++ 21/solve.py
@@ -5,56 +5,7 @@ import numpy as np
 from collections import deque, defaultdict
 from itertools import pairwise
 from itertools import product
-
-
-class VectorTuple(tuple):
-    """
-    This class replicates vectorized operations of numpy arrays, with the
-    advantage that it's hashable.
-    """
-
-    def __new__(cls, *args):
-        if len(args) == 1 and not isinstance(args[0], tuple):
-            args = args[0]
-        return tuple.__new__(VectorTuple, args)
-
-    def __add__(self, other):
-        return VectorTuple(
-            self_element + other_element
-            for self_element, other_element in zip(self, other)
-        )
-
-    def __sub__(self, other):
-        return VectorTuple(
-            self_element - other_element
-            for self_element, other_element in zip(self, other)
-        )
-
-    def __abs__(self):
-        return VectorTuple((abs(element) for element in self))
-
-    def within_range(self, *ranges):
-        return all(element in range_ for element, range_ in zip(self, ranges))
-
-    def orthogonals(self, board):
-        """
-        Generate E, N, W, S adjacencies.
-        """
-        for delta in (
-            VectorTuple(0, 1),
-            VectorTuple(-1, 0),
-            VectorTuple(0, -1),
-            VectorTuple(1, 0),
-        ):
-            next_pos = self + delta
-            if next_pos.within_range(range(board.shape[0]), range(board.shape[1])):
-                yield next_pos
-
-    def manhattan(self):
-        """
-        Get manhattan magnitude of self.
-        """
-        return sum(abs(self))
+from aoc_data_structures import VectorTuple
 
 
 class Keypad:
